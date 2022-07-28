18ff7d2cff5bb555eaa277d6c715482e465b9856
jtphillips215 <jtphillips215@gmail.com>
2022-07-28 18:51:33 -0500

    added player choice function

--- game.js
+++ game.js
@@ -1,6 +1,21 @@
 function getComputerChoice() {
   let ranNum = Math.floor(Math.random() * 3);
-  return ranNum;
+  
+  if (ranNum == 0) {
+    return "Rock";
+  }
+  else if (ranNum == 1) {
+    return "Paper";
+  }
+  else {
+    return "Scissors";
+  }
+}
+
+function getPlayerChoice() {
+  let playerChoice = prompt("Rock, Paper, or Scissors?");
+  return playerChoice;
 }
 
 console.log(getComputerChoice());
+console.log(getPlayerChoice());
