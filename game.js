00ca96904281648411d7e36844222f12d0bde892
jtphillips215 <jtphillips215@gmail.com>
2022-07-28 20:55:25 -0500

    added play round function to game

--- game.js
+++ game.js
@@ -1,14 +1,16 @@
+"use strict";
+
 function getComputerChoice() {
   let ranNum = Math.floor(Math.random() * 3);
   
   if (ranNum == 0) {
-    return "Rock";
+    return "rock";
   }
   else if (ranNum == 1) {
-    return "Paper";
+    return "paper";
   }
   else {
-    return "Scissors";
+    return "scissors";
   }
 }
 
@@ -17,5 +19,45 @@ function getPlayerChoice() {
   return playerChoice;
 }
 
-console.log(getComputerChoice());
-console.log(getPlayerChoice());
+function playRound(computerChoice, playerChoice) {
+  if (computerChoice == "rock") {
+    if(playerChoice.toLowerCase() == "rock") {
+      console.log("Tie Round");
+    }
+    else if (playerChoice.toLowerCase() == "paper") {
+      console.log("Player Win!");
+    }
+    else {
+      console.log("Computer Win!");
+    }
+  }
+  else if (computerChoice == "paper") {
+    if(playerChoice.toLowerCase() == "rock") {
+      console.log("Computer Win!");
+    }
+    else if (playerChoice.toLowerCase() == "paper") {
+      console.log("Tie Round");
+    }
+    else {
+      console.log("Player Win!");
+    }
+  }
+  else {
+    if(playerChoice.toLowerCase() == "rock") {
+      console.log("Player Win!");
+    }
+    else if (playerChoice.toLowerCase() == "paper") {
+      console.log("Computer Win!");
+    }
+    else {
+      console.log("Tie Round");
+    }
+  }
+}
+
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
+let playerChoice = getPlayerChoice();
+console.log(playerChoice);
+playRound(computerChoice, playerChoice);
