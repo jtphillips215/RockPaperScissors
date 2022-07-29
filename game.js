af25165aaff80938967330c8778c79e988253a20
jtphillips215 <jtphillips215@gmail.com>
2022-07-28 22:53:20 -0500

    added game function, console game is at mvp level

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// get computer choice randomly chooses rock, paper, or scissors for the computer player
 function getComputerChoice() {
   let ranNum = Math.floor(Math.random() * 3);
   
@@ -14,50 +15,72 @@ function getComputerChoice() {
   }
 }
 
+// get player choice allows the player to input a choice
 function getPlayerChoice() {
   let playerChoice = prompt("Rock, Paper, or Scissors?");
   return playerChoice;
 }
 
+// play round runs a round of rock paper scissors
 function playRound(computerChoice, playerChoice) {
   if (computerChoice == "rock") {
     if(playerChoice.toLowerCase() == "rock") {
-      console.log("Tie Round");
+      return 0;
     }
     else if (playerChoice.toLowerCase() == "paper") {
-      console.log("Player Win!");
+      return 1;
     }
     else {
-      console.log("Computer Win!");
+      return -1;
     }
   }
   else if (computerChoice == "paper") {
     if(playerChoice.toLowerCase() == "rock") {
-      console.log("Computer Win!");
+      return -1;
     }
     else if (playerChoice.toLowerCase() == "paper") {
-      console.log("Tie Round");
+      return 0;
     }
     else {
-      console.log("Player Win!");
+      return 1;
     }
   }
   else {
     if(playerChoice.toLowerCase() == "rock") {
-      console.log("Player Win!");
+      return 1;
     }
     else if (playerChoice.toLowerCase() == "paper") {
-      console.log("Computer Win!");
+      return -1;
     }
     else {
-      console.log("Tie Round");
+      return 0;
     }
   }
 }
 
+// game runs the game by calling the required functions and keeping score
+function game() {
+  let score = 0;
+  let round = 1
 
-let computerChoice = getComputerChoice();
-console.log(computerChoice);
-let playerChoice = getPlayerChoice();
-console.log(playerChoice);
-playRound(computerChoice, playerChoice);
+  for (let i = 0; i < 5; i++) {
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+    score += playRound(computerChoice, playerChoice);
+    console.log(`Round: ${round}, Computer choice: ${computerChoice}, Player choice: ${playerChoice}`);
+    round++;
+  }
+
+  if(score == 0) {
+    console.log("It's a draw!")
+  }
+  else if (score > 0) {
+    console.log("You Win!")
+  }
+  else {
+    console.log("Computer Wins!")
+  }
+}
+
+// calling game function to start the game
+game();
